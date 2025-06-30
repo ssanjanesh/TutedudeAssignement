761e7f14194274f1951079b896279a0f1106d5d5
ssanjanesh <ssanjanesh@gmail.com>
2025-06-30 21:38:18 +0530

    Add files via upload
    Assignment 1 - Basic Python Concepts

---
+++ Assignment2.py
@@ -0,0 +1,6 @@
+#Create a personalized greeting
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter you last name: ')
+
+print('Hello '+first_name +' ' + last_name +'! Welcome to the Python Program')
