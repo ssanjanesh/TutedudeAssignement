fa99aae12cd895d7815c0106449f432e7c7654c1
ssanjanesh <ssanjanesh@gmail.com>
2025-07-07 22:30:47 +0530

    Delete Assignment2.py

--- Assignment2.py
+++
@@ -1,6 +0,0 @@
-#Create a personalized greeting
-
-first_name = input('Enter your first name: ')
-last_name = input('Enter you last name: ')
-
-print('Hello '+first_name +' ' + last_name +'! Welcome to the Python Program')
